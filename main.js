a817ea3f8ab6df1ece710c7da1b4be8454d28d0f
Abdelrhman Essam <89538266+AbdelrhmanEssam74@users.noreply.github.com>
2024-02-11 18:37:04 +0200

    Update main.js
        Check if the width is 767px or less and customize the menu in mobile view

--- main.js
+++ main.js
@@ -29,3 +29,43 @@ function hidePassword(e) {
     eyeHidePassword.style.display = "none";
     passwordInput.type = "password";
 }
+
+//NOTE - toggle_menu
+//menu icon
+const menuicon = document.querySelector(".toggle_menu");
+//menu
+const menu = document.getElementById("menu");
+menuicon.addEventListener("click", () => {
+    menu.classList.toggle("menu");
+});
+
+
+
+var newmenu = document.getElementById("menu");
+var newNav = `
+<li> <a href="#">للبيع</a></li>
+<li> <a href="#">للإيجار</a></li>
+<li> <a href="#">للطلاب</a></li>
+<li> <a href="#">تجاريه</a></li>
+<li> <a href="#">عنا</a></li>
+<li><a href="#">تواصل معنا</a></li>
+`;
+// Check the width of the website on the window resize
+window.addEventListener('resize', function () {
+    var websiteWidth = window.innerWidth || document.documentElement.clientWidth;
+
+    // Check if the width is 767px or less
+    if (websiteWidth <= 767) {
+        newmenu.innerHTML = newNav;
+    } else {
+        newmenu.innerHTML = `                    
+        <li><a href="#">تواصل معنا</a></li>
+        <li> <a href="#">عنا</a></li>
+        <li> <a href="#">تجاريه</a></li>
+        <li> <a href="#">للطلاب</a></li>
+        <li> <a href="#">للإيجار</a></li>
+        <li> <a href="#">للبيع</a></li>
+        `;
+        // Reset the actions if the width is greater than 767px
+    }
+});
