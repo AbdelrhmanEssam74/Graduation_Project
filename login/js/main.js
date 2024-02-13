daad2bf29a4a23d7b14aa9b623a558acadda4ff7
Abdelrhman Essam <89538266+AbdelrhmanEssam74@users.noreply.github.com>
2024-02-13 14:44:45 +0200

    Add files via upload

---
+++ login/js/main.js
@@ -0,0 +1,61 @@
+
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+
+
+
+//NOTE - show and hide the password
+var eyeShowPassword = document.querySelector('#eyeShowPassword');
+var eyeHidePassword = document.querySelector('#eyeHidePassword');
+
+var passwordInput = document.querySelector('#password');
+eyeShowPassword.addEventListener("click", showPassword);
+eyeHidePassword.addEventListener("click", hidePassword);
+function showPassword(e) {
+    eyeHidePassword.style.display = "block";
+    eyeShowPassword.style.display = "none";
+    passwordInput.type = "text";
+}
+function hidePassword(e) {
+    eyeShowPassword.style.display = "block";
+    eyeHidePassword.style.display = "none";
+    passwordInput.type = "password";
+}
+
+//NOTE - toggle_menu
+//menu icon
+const menuicon = document.querySelector(".toggle_menu");
+//menu
+const menu = document.getElementById("menu");
+menuicon.addEventListener("click", () => {
+    menu.classList.toggle("menu");
+});
+
+
+var newmenu = document.getElementById("menu");
+var newNav = `
+<li> <a href="#">للبيع</a></li>
+<li> <a href="#">للإيجار</a></li>
+<li> <a href="#">للطلاب</a></li>
+<li> <a href="#">تجاريه</a></li>
+<li> <a href="#">عنا</a></li>
+<li><a href="#">تواصل معنا</a></li>
+`;
+// Check the width of the website on window resize
+window.addEventListener('resize', function () {
+    var websiteWidth = window.innerWidth || document.documentElement.clientWidth;
+
+    // Check if the width is 767px or less
+    if (websiteWidth <= 767) {
+        newmenu.innerHTML = newNav;
+    } else {
+        newmenu.innerHTML = `                    
+        <li><a href="#">تواصل معنا</a></li>
+        <li> <a href="#">عنا</a></li>
+        <li> <a href="#">تجاريه</a></li>
+        <li> <a href="#">للطلاب</a></li>
+        <li> <a href="#">للإيجار</a></li>
+        <li> <a href="#">للبيع</a></li>
+        `;
+    }
+});
