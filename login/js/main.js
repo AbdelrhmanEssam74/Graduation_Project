dcc2cae98c1cbc2f009665124e60d28287538942
Abdelrhman Essam <89538266+AbdelrhmanEssam74@users.noreply.github.com>
2024-02-14 17:37:34 +0200

    Update main.js

--- login/js/main.js
+++ login/js/main.js
@@ -1,7 +1,69 @@
+//SECTION -  start form validation
 
-    var email = document.getElementById("email");
-    var password = document.getElementById("password");
+let form = document.getElementById('form');
+let email = document.getElementById("email");
+let password = document.getElementById("password");
 
+form.addEventListener("submit", (e) => {
+    validateInputs();
+    let result = isFormValid();
+    if (result == true) {
+        form.submit();
+    } else {
+        e.preventDefault();
+    }
+});
+const setErrors = (e, m) => {
+    const inputControl = e.parentElement;
+    const errorDisplay = inputControl.querySelector(".error_message");
+    const input_icon = inputControl.querySelector("i");
+    errorDisplay.innerText = m;
+    errorDisplay.style.display = "block";
+    e.style.border = "1px solid var(--danger)"
+    input_icon.style.color = "var(--danger)";
+    form.addEventListener("click", () => {
+        errorDisplay.style.display = "none";
+    });
+
+}
+
+
+const setSuccess = (e) => {
+    const inputControl = e.parentElement;
+    const errorDisplay = inputControl.querySelector(".error_message");
+    const input_icon = inputControl.querySelector("i");
+    errorDisplay.style.display = "none";
+    e.style.border = "1px solid var(--success)"
+    input_icon.style.color = "var(--success)";
+    errorDisplay.innerText = "";
+};
+const validateInputs = () => {
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    if (emailValue === "") {
+        setErrors(email, "ادخل بريدك الالكتروني")
+    } else {
+        setSuccess(email);
+    }
+    if (passwordValue === "") {
+        setErrors(password, "ادخل كلمة السر")
+    } else {
+        setSuccess(password);
+    }
+}
+
+
+function isFormValid() {
+    const error = form.querySelectorAll(".error_message");
+    let result = true;
+    error.forEach((container) => {
+        if (container.style.display === "block") {
+            result = false;
+        }
+    });
+    return result;
+}
+//SECTION -  end form validation
 
 
 //NOTE - show and hide the password
