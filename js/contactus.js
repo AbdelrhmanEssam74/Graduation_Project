a7522788c8697c5adcb5f45fae2d54c7dd79a94c
Abdelrhman Essam <89538266+AbdelrhmanEssam74@users.noreply.github.com>
2024-03-06 01:17:11 +0200

    Add files via upload

--- js/contactus.js
+++ js/contactus.js
@@ -14,3 +14,31 @@ setTimeout(() => {
     }
 }, 5000);
 
+
+// animate navbar
+window.addEventListener("scroll", function () {
+    let header = document.querySelector("header")
+    header.classList.toggle("sticky", window.scrollY);
+
+});
+
+//button to go to the top of the page
+//get the button element
+const btn = document.querySelector(".up");
+//using windo.onscroll() method
+document.onscroll = () => {
+    if (this.scrollY >= 400) {
+        //add className to the butoon "show"
+        btn.classList.add("show");
+    } else {
+        //remove className to the butoon "show"
+        btn.classList.remove("show");
+    }
+};
+//then click on the button to go to the top of page
+btn.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+};
